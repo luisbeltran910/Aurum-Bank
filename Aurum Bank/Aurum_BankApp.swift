6e60f0919c6a9aa63411cc8e8ff79b6b5f3ad17e
luisbeltran910 <luisbeltran991@gmail.com>
2026-05-10 19:10:04 -0700

    Initial Commit

---
+++ Aurum Bank/Aurum_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aurum_BankApp.swift
+//  Aurum Bank
+//
+//  Created by Luis Beltran on 10/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Aurum_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aurum Bank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aurum Bank
+//
+//  Created by Luis Beltran on 10/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
